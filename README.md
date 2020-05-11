4e5757b859620253f3dd3fe0fcea10bc110067d8
Yangseok Lee <ysl0715@gmail.com>
2020-05-11 18:00:16 +0900

    .

---
+++ README.md
@@ -0,0 +1,7 @@
+git clone https://github.com/Yangseok/ubuntu-apm-install.git
+
+cd ubuntu-apm-install
+
+chmod +x install.sh
+
+./install.sh
---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+sudo apt install -y apache2
+sudo systemctl enable apache2.service
+sudo systemctl restart apache2.service
+
+sudo apt install -y mariadb-server
+echo " -- set db password -- enter/n/y/n/n/y"
+sudo mysql_secure_installation
+# enter
+#sudo mysql -uroot -p
+#add db, add account, grant flush ...
+#sudo vi /etc/mysql/mariadb.conf.d/50-server.cnf
+#bind-address           = 127.0.0.1 change
+#bind-address           = 0.0.0.0
+sudo systemctl status mariadb.service
+sudo systemctl enable mariadb.service
+sudo systemctl restart mariadb.service
+
+sudo apt install -y php php-bz2 php-common php-curl php-date php-xml php-gd php-gettext php-json php-mbstring php-mysql php-readline php-soap php-sqlite3 php-tokenizer php-xml php-xmlrpc
+
+sudo apt install -y php-dev libmcrypt-dev gcc make autoconf libc-dev pkg-config
+sudo pecl install mcrypt-1.0.1
+# enter
+sudo echo "extension=mcrypt.so" | sudo tee -a /etc/php/7.2/apache2/conf.d/mcrypt.ini
+
+sudo apt install -y php-mysql
+sudo systemctl restart apache2.service
+
+#Let’s Encrypt
+#https://hiseon.me/server/letsencrypt-ssl-certificate/ 
+sudo apt-get update
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository ppa:certbot/certbot
+sudo apt-get update
+sudo apt-get install -y python-certbot-apache 
+sudo certbot --apache certonly
+#add domain
+sudo a2enmod ssl
