aeb3c9ed45bd0ce9a4c3730ba0dfc042264e5640
Yangseok <ysl8607@naver.com>
2020-06-14 10:00:45 +0900

    unzip, php-cli

--- install.sh
+++ install.sh
@@ -4,20 +4,22 @@ sudo apt install -y apache2
 sudo systemctl enable apache2.service
 sudo systemctl restart apache2.service
 
-sudo apt install -y mariadb-server
-echo " -- set db password -- enter/n/y/n/n/y"
-sudo mysql_secure_installation
+### mariadb setup
+##sudo apt install -y mariadb-server
+##echo " -- set db password -- enter/n/y/n/n/y"
+##sudo mysql_secure_installation
+
 # enter
 #sudo mysql -uroot -p
 #add db, add account, grant flush ...
 #sudo vi /etc/mysql/mariadb.conf.d/50-server.cnf
 #bind-address           = 127.0.0.1 change
 #bind-address           = 0.0.0.0
-sudo systemctl status mariadb.service
-sudo systemctl enable mariadb.service
-sudo systemctl restart mariadb.service
+##sudo systemctl status mariadb.service
+##sudo systemctl enable mariadb.service
+##sudo systemctl restart mariadb.service
 
-sudo apt install -y php php-bz2 php-common php-curl php-date php-xml php-gd php-gettext php-json php-mbstring php-mysql php-readline php-soap php-sqlite3 php-tokenizer php-xml php-xmlrpc
+sudo apt install -y php php-bz2 php-common php-cli php-curl php-date php-xml php-gd php-gettext php-json php-mbstring php-mysql php-readline php-soap php-sqlite3 php-tokenizer php-xml php-xmlrpc unzip
 
 sudo apt install -y php-dev libmcrypt-dev gcc make autoconf libc-dev pkg-config
 sudo pecl install mcrypt-1.0.1
